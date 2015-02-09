6c7fc626a6d39ccf91a4e966a686dfa4f6e2038a
John Jenkinson <john.jenkinson@utsa.edu>
2015-02-09 08:40:34 -0600

    Create GreenChannelExtraction.m

---
+++ GreenChannelExtraction.m
@@ -0,0 +1,10 @@
+% call GreenChannelExtraction.m
+% John Jenkinson, UTSA ECE October 26, 2014
+%
+% Extract the Green channel from the image.
+%
+function[fGreen]=GreenChannelExtraction(f)
+    
+    fGreen=f(:,:,2);
+    
+end
